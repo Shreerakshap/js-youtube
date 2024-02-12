105dcf8dbc306ccadde1eb973604b953034581d2
Shreerakshap <159769586+Shreerakshap@users.noreply.github.com>
2024-02-12 10:33:46 +0000

    variables and const demo

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 144553
+let accountEmail="s@gmail.com"
+var accountPassword="12345";
+accountCity="Jaipur";
+
+// accountId=2; //not allowed
+accountEmail="a@gmail.com"
+accountPassword="21212121"
+accountCity="Bangalore";
+console.log(accountId);
+// console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity])
