f4e2ce68ff59c2e10c2ece1dc84412e096fc655c
Shreerakshap <159769586+Shreerakshap@users.noreply.github.com>
2024-02-17 06:42:42 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+// alert("hello"); // we are using node.js not browser
+console.log(3 + 3); //code readability should be high
+console.log("Hitesh");
+
+
+let name="Hitesh";
+let age=18
+let isLoggedIn=true;
+
+//PRIMITIVE DATATYPES
+
+//number - 2 power 53
+//bigint
+//string 
+//boolean  -> true or false
+//null -> standalone value
+//undefined-> value not assigned
+//symbol - uniqueness
+
+//object
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined);
