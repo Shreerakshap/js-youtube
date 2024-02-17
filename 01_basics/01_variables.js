a875febc8cc473023dee42b6da69462bd19d4f7c
Shreerakshap <159769586+Shreerakshap@users.noreply.github.com>
2024-02-17 06:23:14 +0000

    variables and constants- var let const

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,12 +1,16 @@
 const accountId= 144553
 let accountEmail="s@gmail.com"
 var accountPassword="12345";
-accountCity="Jaipur";
-
+accountCity="Jaipur";//not a good practise
+let accountState;
 // accountId=2; //not allowed
 accountEmail="a@gmail.com"
 accountPassword="21212121"
 accountCity="Bangalore";
 console.log(accountId);
+
+/*
+Prefer not to use var bacause of issues in block scope and functional scope
+*/
 // console.log(accountEmail);
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
