dc67e05cc3412d77e2a05dfb8d695caf7cb5831e
Shreerakshap <159769586+Shreerakshap@users.noreply.github.com>
2024-02-17 07:10:09 +0000

    operation basics 1

--- 01_basics/03_conversion_operations.js
+++ 01_basics/03_conversion_operations.js
@@ -29,3 +29,21 @@ let someNumber= 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// **************************Operations*************************
+let value=3;
+let negValue= -value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);//power
+
+let str1= "hello"
+let str2=" hitesh";
+let str3= str1+str2;
+console.log(str3);
+
